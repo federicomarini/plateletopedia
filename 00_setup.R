d8f4db0f11bfbc3ad2fbe3a7d1ebfaf8c283fea0
Federico Marini <marinif@uni-mainz.de>
2018-06-18 14:46:39 +0200

    added tx2gene objects, and GRanges objects for the annotations in use

--- 00_setup.R
+++ 00_setup.R
@@ -254,6 +254,207 @@ makeEnsembldbPackage(ensdb = dbfile(dbconn(edb_v92_mouse)),
 
 
 
+## -------------------------------------------------------------------------- ##
+##  Generating tx2gene mappings and GRanges objects                    ------
+## -------------------------------------------------------------------------- ##
+
+# First, the tx2gene objects
+
+# ercc <- readDNAStringSet(ercc_fa)
+# ercc <- data.frame(tx = names(ercc), gene = names(ercc), stringsAsFactors = FALSE)
+
+# using the excellent code from https://github.com/markrobinsonuzh/conquer/blob/master/01_build_index.R
+library("Biostrings")
+
+## Human
+cdna <- readDNAStringSet("_ref/ENSEMBL_release92/Homo_sapiens.GRCh38.cdna.all.fa")
+ncrna <- readDNAStringSet("_ref/ENSEMBL_release92/Homo_sapiens.GRCh38.ncrna.fa")
+cdna_df <- data.frame(t(sapply(as.character(names(cdna)), function(nm) {
+  a <- strsplit(nm, " ")[[1]]
+  tx <- a[1]
+  gene <- gsub("^gene:", "", a[grep("^gene:", a)])
+  gene_symbol <- gsub("^gene_symbol:", "", a[grep("^gene_symbol:", a)])
+  c(tx = ifelse(length(tx) != 0, tx, NA), 
+    gene = ifelse(length(gene) != 0, gene, NA), 
+    gene_symbol = ifelse(length(gene_symbol) != 0, gene_symbol, NA))
+})), stringsAsFactors = FALSE)
+ncrna_df <- data.frame(t(sapply(as.character(names(ncrna)), function(nm) {
+  a <- strsplit(nm, " ")[[1]]
+  tx <- a[1]
+  gene <- gsub("^gene:", "", a[grep("^gene:", a)])
+  gene_symbol <- gsub("^gene_symbol:", "", a[grep("^gene_symbol:", a)])
+  c(tx = ifelse(length(tx) != 0, tx, NA), 
+    gene = ifelse(length(gene) != 0, gene, NA), 
+    gene_symbol = ifelse(length(gene_symbol) != 0, gene_symbol, NA))
+})), stringsAsFactors = FALSE)
+txgenemap <- rbind(cdna_df, ncrna_df) # , ercc)
+saveRDS(txgenemap, 
+        file = "_ref/tx2gene_Homo_sapiens.GRCh38.92.cdna.ncrna.txgenemap.rds")
+
+## Mouse
+cdna <- readDNAStringSet("_ref/ENSEMBL_release92/Mus_musculus.GRCm38.cdna.all.fa")
+ncrna <- readDNAStringSet("_ref/ENSEMBL_release92/Mus_musculus.GRCm38.ncrna.fa")
+cdna_df <- data.frame(t(sapply(as.character(names(cdna)), function(nm) {
+  a <- strsplit(nm, " ")[[1]]
+  tx <- a[1]
+  gene <- gsub("^gene:", "", a[grep("^gene:", a)])
+  gene_symbol <- gsub("^gene_symbol:", "", a[grep("^gene_symbol:", a)])
+  c(tx = ifelse(length(tx) != 0, tx, NA), 
+    gene = ifelse(length(gene) != 0, gene, NA), 
+    gene_symbol = ifelse(length(gene_symbol) != 0, gene_symbol, NA))
+})), stringsAsFactors = FALSE)
+ncrna_df <- data.frame(t(sapply(as.character(names(ncrna)), function(nm) {
+  a <- strsplit(nm, " ")[[1]]
+  tx <- a[1]
+  gene <- gsub("^gene:", "", a[grep("^gene:", a)])
+  gene_symbol <- gsub("^gene_symbol:", "", a[grep("^gene_symbol:", a)])
+  c(tx = ifelse(length(tx) != 0, tx, NA), 
+    gene = ifelse(length(gene) != 0, gene, NA), 
+    gene_symbol = ifelse(length(gene_symbol) != 0, gene_symbol, NA))
+})), stringsAsFactors = FALSE)
+txgenemap <- rbind(cdna_df, ncrna_df) # , ercc)
+saveRDS(txgenemap, 
+        file = "_ref/tx2gene_Mus_musculus.GRCm38.92.cdna.ncrna.txgenemap.rds")
+
+
+# Now, the GRanges objects
+
+library(GenomicRanges)
+library(dplyr)
+
+## Human
+cdna <- readDNAStringSet("_ref/ENSEMBL_release92/Homo_sapiens.GRCh38.cdna.all.fa")
+ncrna <- readDNAStringSet("_ref/ENSEMBL_release92/Homo_sapiens.GRCh38.ncrna.fa")
+
+nm <- c(names(cdna), names(ncrna))
+info <- data.frame(t(sapply(nm, function(w) {
+  w <- strsplit(w, " ")[[1]]
+  transcript <- w[1]
+  gene <- gsub("gene:", "", w[grep("^gene:", w)])
+  position <- gsub("chromosome:", "", w[grep("^chromosome:", w)])
+  if (length(position) == 0) position <- gsub("scaffold:", "", w[grep("^scaffold:", w)])
+  symbol <- gsub("gene_symbol:", "", w[grep("^gene_symbol", w)])
+  c(transcript = transcript, 
+    gene = gene,
+    genome = strsplit(position, ":")[[1]][1], 
+    chromosome = strsplit(position, ":")[[1]][2],
+    start = strsplit(position, ":")[[1]][3],
+    end = strsplit(position, ":")[[1]][4],
+    strand = ifelse(strsplit(position, ":")[[1]][5] == "1", "+", "-"),
+    symbol = symbol
+  )
+})), stringsAsFactors = FALSE)
+rownames(info) <- NULL
+info$start <- as.numeric(info$start)
+info$end <- as.numeric(info$end)
+
+txgr <- GRanges(seqnames = info$chromosome, 
+                ranges = IRanges(start = info$start, end = info$end), 
+                strand = info$strand)
+mcols(txgr) <- info[, c("transcript", "gene", "genome", "symbol")]
+
+# erccgtf <- import(ercc_gtf, format = "gtf")
+# ercctx <- erccgtf
+# mcols(ercctx) <- data.frame(transcript = ercctx$gene_id,
+#                             gene = ercctx$gene_id,
+#                             genome = ercctx$source,
+#                             symbol = ercctx$gene_id)
+# 
+# txgr <- suppressWarnings(c(txgr, ercctx))
+names(txgr) <- txgr$transcript
+
+geneinfo <- info %>% group_by(gene) %>% 
+  summarise(genome = unique(genome),
+            chromosome = unique(chromosome),
+            start = min(start),
+            end = max(end),
+            strand = unique(strand),
+            symbol = unique(symbol)) %>% 
+  ungroup()
+
+ggr <- GRanges(seqnames = geneinfo$chromosome,
+               ranges = IRanges(start = geneinfo$start, end = geneinfo$end),
+               strand = geneinfo$strand)
+mcols(ggr) <- geneinfo[, c("gene", "genome", "symbol")]
+
+# erccgene <- erccgtf
+# mcols(erccgene) <- data.frame(gene = erccgene$gene_id,
+#                               genome = erccgene$source,
+#                               symbol = erccgene$gene_id)
+# ggr <- suppressWarnings(c(ggr, erccgene))
+# names(ggr) <- ggr$gene
+
+gene_granges <- ggr
+tx_granges <- txgr
+saveRDS(list(gene_granges = gene_granges, tx_granges = tx_granges),
+        file = "_ref/granges_Homo_sapiens.GRCh38.92.cdna.ncrna.granges.gene_tx.rds")
+
+## Mouse
+cdna <- readDNAStringSet("_ref/ENSEMBL_release92/Mus_musculus.GRCm38.cdna.all.fa")
+ncrna <- readDNAStringSet("_ref/ENSEMBL_release92/Mus_musculus.GRCm38.ncrna.fa")
+
+nm <- c(names(cdna), names(ncrna))
+info <- data.frame(t(sapply(nm, function(w) {
+  w <- strsplit(w, " ")[[1]]
+  transcript <- w[1]
+  gene <- gsub("gene:", "", w[grep("^gene:", w)])
+  position <- gsub("chromosome:", "", w[grep("^chromosome:", w)])
+  if (length(position) == 0) position <- gsub("scaffold:", "", w[grep("^scaffold:", w)])
+  symbol <- gsub("gene_symbol:", "", w[grep("^gene_symbol", w)])
+  c(transcript = transcript, 
+    gene = gene,
+    genome = strsplit(position, ":")[[1]][1], 
+    chromosome = strsplit(position, ":")[[1]][2],
+    start = strsplit(position, ":")[[1]][3],
+    end = strsplit(position, ":")[[1]][4],
+    strand = ifelse(strsplit(position, ":")[[1]][5] == "1", "+", "-"),
+    symbol = symbol
+  )
+})), stringsAsFactors = FALSE)
+rownames(info) <- NULL
+info$start <- as.numeric(info$start)
+info$end <- as.numeric(info$end)
+
+txgr <- GRanges(seqnames = info$chromosome, 
+                ranges = IRanges(start = info$start, end = info$end), 
+                strand = info$strand)
+mcols(txgr) <- info[, c("transcript", "gene", "genome", "symbol")]
+
+# erccgtf <- import(ercc_gtf, format = "gtf")
+# ercctx <- erccgtf
+# mcols(ercctx) <- data.frame(transcript = ercctx$gene_id,
+#                             gene = ercctx$gene_id,
+#                             genome = ercctx$source,
+#                             symbol = ercctx$gene_id)
+# 
+# txgr <- suppressWarnings(c(txgr, ercctx))
+names(txgr) <- txgr$transcript
+
+geneinfo <- info %>% group_by(gene) %>% 
+  summarise(genome = unique(genome),
+            chromosome = unique(chromosome),
+            start = min(start),
+            end = max(end),
+            strand = unique(strand),
+            symbol = unique(symbol)) %>% 
+  ungroup()
+
+ggr <- GRanges(seqnames = geneinfo$chromosome,
+               ranges = IRanges(start = geneinfo$start, end = geneinfo$end),
+               strand = geneinfo$strand)
+mcols(ggr) <- geneinfo[, c("gene", "genome", "symbol")]
+
+# erccgene <- erccgtf
+# mcols(erccgene) <- data.frame(gene = erccgene$gene_id,
+#                               genome = erccgene$source,
+#                               symbol = erccgene$gene_id)
+# ggr <- suppressWarnings(c(ggr, erccgene))
+# names(ggr) <- ggr$gene
+
+gene_granges <- ggr
+tx_granges <- txgr
+saveRDS(list(gene_granges = gene_granges, tx_granges = tx_granges),
+        file = "_ref/granges_Mus_musculus.GRCm38.92.cdna.ncrna.granges.gene_tx.rds")
 
 
 
